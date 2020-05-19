a324012bef9788da234483e1e8649b34dc208c5d
mtodero <mtodero@gmail.com>
2020-05-18 23:43:40 -0300

    Update get_sidra.R

--- R/get_sidra.R
+++ R/get_sidra.R
@@ -398,7 +398,7 @@ get_sidra <- function(x,
 
   test1 <- try(rjson::fromJSON(path), silent=TRUE)
   
-  msg_path <- paste0("Full path -> ", path)
+  msg_path <- paste0("Trace -> ", strsplit(path, " ")[[1]])
   message(msg_path)
   if (strsplit(path, " ")[[1]][2] == "P") {
     
